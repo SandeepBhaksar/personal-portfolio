1d84090b70b3ffec73d2f086e9610b042bcd92c1
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2024-10-07 13:06:25 +0530

    download correction

--- src/assets/Pages/Navbar/Navbar.jsx
+++ src/assets/Pages/Navbar/Navbar.jsx
@@ -2,29 +2,30 @@ import React from 'react';
 import logo from '/logo.png';
 import './Navbar.css';
 import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
+import Resume from '../../../CV/Sandeep_Bhaskar_Resume.pdf'; // Ensure the filename is correct
 
 const Navbar = () => {
   return (
     <div className="navbar">
-    <div className='nav-left'>
+      <div className='nav-left'>
         <img src={logo} alt='logo' className='nav-logo' />
-    </div>
+      </div>
 
-    <div className="nav-right">
-      <ul>
-        <li>About</li>
-        <li>Work</li>
-        <li>Contact</li>
-        <li><ThemeSwitcher/></li>
-        <li>
-        <a href="./CV/Sandeep_Bhaskar_Resume.pdf" download="Sandeep_Bhaskar_Resume.pdf" className='cv-link'>
-            <button className='cv-dwnld'>Download CV</button>
-        </a>
-        </li>
-      </ul>
+      <div className="nav-right">
+        <ul>
+          <li>About</li>
+          <li>Work</li>
+          <li>Contact</li>
+          <li><ThemeSwitcher /></li>
+          <li>
+            <a href={Resume} className='cv-link' download> {/* Added download attribute */}
+              <button className='cv-dwnld'>Download CV</button>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
