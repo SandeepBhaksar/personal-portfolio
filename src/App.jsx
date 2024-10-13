76f1cef905e00f780b490c4d1102f54ead3fcf01
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2024-10-13 12:52:49 +0530

    skills

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Home from './assets/Pages/Home/Home';
 import AboutMe from './assets/Pages/AboutMe/AboutMe';
 import Contact from './assets/Pages/Contact/Contact';
+import Skills from './assets/Pages/Skills/Skills';
 
 const App = () => {
   return (
     <div>
       <Home />
       <AboutMe/>
+      <Skills />
       <Contact />
     </div>
   )
--- src/assets/Pages/Navbar/Navbar.jsx
+++ src/assets/Pages/Navbar/Navbar.jsx
@@ -5,6 +5,19 @@ import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
 import Resume from '../../../CV/Sandeep_Bhaskar_Resume.pdf';
 
 const Navbar = () => {
+
+  const handleScroll = (event, targetId) => {
+    event.preventDefault();
+    const targetElement = document.querySelector(targetId);
+    const offset = document.querySelector('.navbar').offsetHeight; 
+    const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - offset;
+
+    window.scrollTo({
+      top: targetPosition,
+      behavior: 'smooth'
+    });
+  };
+
   return (
     <div className="navbar">
       <div className='nav-left'>
@@ -12,9 +25,9 @@ const Navbar = () => {
       </div>
 
       <div className="nav-right">
-        <a href="#about" className='li-component'>About</a>
-        <a href="#work" className='li-component'>Work</a>
-        <a href="#contact" className='li-component'>Contact</a>
+        <a href="#about" className='li-component' onClick={(e) => handleScroll(e, '#about')}>About</a>
+        <a href="#work" className='li-component' onClick={(e) => handleScroll(e, '#work')}>Work</a>
+        <a href="#contact" className='li-component' onClick={(e) => handleScroll(e, '#contact')}>Contact</a>
         <a href="#" className='li-component'><ThemeSwitcher /></a>
         <a href={Resume} className='cv-link' download>
           <button className='cv-dwnld'>Download CV</button>
